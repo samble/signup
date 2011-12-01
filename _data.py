90d0e61f490b717957f4b1a37e3c472c300042f4
sam b <samble@saga.silenceisdefeat.net>
2011-12-01 17:07:38 -0600

    new columns for some custom values in the db and _data file recording columns that were already there

--- _data.py
+++ _data.py
@@ -1,8 +1,4 @@
 
-QUERY_DELETE_INCOMING = 'DELETE FROM IncomingPaypal WHERE id = ?'
-
-QUERY_INSERT_STATUS = 'INSERT INTO IncomingPaypalStatus (history_id, status) VALUES (:history_id, :status)'
-
 PAYPAL_FIELDS = ['last_name',
 'txn_id',
 'receiver_email',
@@ -51,7 +47,9 @@ PAYPAL_FIELDS = ['last_name',
 'mc_gross',
 'mc_shipping1',
 'quantity',
-'subscr_id']
+'subscr_id',
+'option_selection1',
+'option_name1']
 
 PAYPAL_HISTORY_FIELDS = PAYPAL_FIELDS
 PAYPAL_INCOMING_FIELDS = PAYPAL_HISTORY_FIELDS + ['post_body','history_id']
@@ -64,43 +62,27 @@ QUERY_INSERT_PAYPAL_INCOMING = 'INSERT INTO IncomingPaypal (' + \
                         ','.join(PAYPAL_INCOMING_FIELDS) + ') VALUES (' + \
                         ','.join([':' + field for field in PAYPAL_INCOMING_FIELDS]) + ')'
 
-QUERY_GET_INCOMING = '''SELECT
+QUERY_GET_PAYPAL_INCOMING = '''SELECT
 id,
 history_id,
+txn_id,
 receiver_email,
 payment_status,
-item_name1,
 item_name,
-charset,
-parent_txn_id,
-custom,
-notify_version,
-address_name,
-for_auction,
-mc_gross_1,
 test_ipn,
-item_number,
-receiver_id,
-pending_reason,
-business,
 payer_id,
-mc_handling1,
-auction_closing_date,
-mc_handling,
-auction_buyer_id,
-address_zip,
-address_city,
-receipt_ID,
-mc_fee,
-mc_currency,
-shipping,
-verify_sign,
 payer_email,
-payment_type,
-mc_gross,
-mc_shipping1,
-quantity,
-subscr_id
+option_selection1,
+option_name1,
+post_body
 FROM IncomingPaypal
-WHERE id = (SELECT MIN(id) FROM IncomingPaypal)
-'''
+WHERE id = (SELECT MIN(id) FROM IncomingPaypal)'''
+
+QUERY_DELETE_PAYPAL_INCOMING = 'DELETE FROM IncomingPaypal WHERE id = ?'
+
+QUERY_INSERT_PAYPAL_STATUS = 'INSERT INTO IncomingPaypalStatus (history_id, status) VALUES (:history_id, :status)' 
+
+QUERY_UPDATE_PAYPAL_STATUS = 'UPDATE IncomingPaypalStatus SET status = :status, update_date = current_timestamp where history_id = :history_id'
+
+
+PAYPAL_VERIFY_URL = 'http://www.sandbox.paypal.com/cgi-bin/webscr'
--- transactionPaypalProcessor.py
+++ transactionPaypalProcessor.py
@@ -2,22 +2,55 @@
 """ module doc string """
 import sqlite3
 
+from urllib2 import urlopen
+
 from _include import SQLITE_DB_PATH
-from _data import (QUERY_GET_INCOMING,
-                   QUERY_DELETE_INCOMING,
-                   QUERY_INSERT_STATUS)
+from _data import (QUERY_GET_PAYPAL_INCOMING,
+                   QUERY_DELETE_PAYPAL_INCOMING,
+                   QUERY_INSERT_PAYPAL_STATUS)
 
 def process():
-    """ NOTE: ``a`` is undefined currently.. """
     con = sqlite3.connect(SQLITE_DB_PATH)
     con.row_factory = sqlite3.Row
     cur = con.cursor()
 
     cur.execute('begin exclusive transaction')
 
-    cur.execute(QUERY_GET_INCOMING)
+    cur.execute(QUERY_GET_PAYPAL_INCOMING)
     incomingRow = cur.fetchone()
-    cur.execute(QUERY_INSERT_STATUS, a)
-    cur.execute(QUERY_DELETE_INCOMING, (incomingRow['id']))
+    cur.execute(QUERY_INSERT_PAYPAL_STATUS, 
+        {'history_id': incomingRow['history_id'], 
+         'status': 'initial'})
+    cur.execute(QUERY_DELETE_PAYPAL_INCOMING, (incomingRow['id'],))
 
     cur.commit()
+    con.close()
+
+    history_id = incomingRow['history_id']
+    post_body = incomingRow['post_body']
+
+    if incomingRow['payment_status'] != 'Completed':
+        updateStatus(history_id, 'incomplete payment')
+    elif item_name not in ['Shell Account', 'Donation']:
+        updateStatus(history_id, 'unknown item_name')
+    elif not paypalVerifyPost(post_body):
+        updateStatus(history_id, 'verification failed')
+
+def updateStatus(history_id, status):
+    try:
+        con = sqlite3.connect(SQLITE_DB_PATH)
+        con.execute(QUERY_UPDATE_PAYPAL_STATUS, {'history_id': history_id, 'status': status})        
+        con.close()
+    except Exception as ex:
+        exInfo = traceback.format_exc()
+        logException(ex, exInfo)
+
+def paypalVerifyPost(post_body):
+    try:
+        urlopen(
+    except Exception as ex:
+        exInfo = traceback.format_exc()
+        logException(ex, exInfo)
+        return False
+    else:
+        return True
